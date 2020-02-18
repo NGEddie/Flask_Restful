67e7494f38b395bcada0aeeb48be40917da20f5c
Nigel Gormley <nigel.gormley@archlinode>
2020-02-18 19:37:27 +0000

    changed settings to use postgres

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from datetime import timedelta
 from flask import Flask
 from flask_restful import Api
@@ -10,7 +12,8 @@ from resources.store import Store, Stores
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
+                                                       'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'nigel'
 api = Api(app)
